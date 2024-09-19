976e07f7e330bcb05a5fdd3ff9f07daa5afa9a68
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-09-19 17:51:21 -0400

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,50 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import "./globals.css"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+})
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+})
 
 export const metadata: Metadata = {
   title: "Jordan's Portfolio!",
   description: "I'm a software engineer who loves to build!",
-};
+  icons: {
+    icon: "/favicon/favicon.ico", // Main favicon
+    shortcut: "/favicon/favicon-16x16.png", // Shortcut icon
+    apple: "/favicon/apple-touch-icon.png", // Apple touch icon
+    other: [
+      {
+        rel: "icon",
+        url: "/favicon/favicon-32x32.png", // Additional favicon sizes
+        sizes: "32x32",
+      },
+      {
+        rel: "icon",
+        url: "/favicon/android-chrome-192x192.png", // Android icon
+        sizes: "192x192",
+      },
+      {
+        rel: "icon",
+        url: "/favicon/android-chrome-512x512.png", // Android icon
+        sizes: "512x512",
+      },
+    ],
+  },
+  manifest: "/favicon/site.webmanifest",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -31,5 +54,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  );
+  )
 }
